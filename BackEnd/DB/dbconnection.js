ba84ee1165702f8666a6b9fbe027898eddcc7548
Sebastian <sebastian.sierrav2001@gmail.com>
2022-07-21 15:29:21 -0500

    ya usa variables de entorno

--- BackEnd/DB/dbconnection.js
+++ BackEnd/DB/dbconnection.js
@@ -1,19 +1,21 @@
 import mysql from 'mysql';
 
-/*
+
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST || 'localhost',
+    host: process.env.MYSQL_HOST || '3.83.110.245',
     user: process.env.MYSQL_USER || 'nube',
     password: process.emv.MYSQL_PASSWORD || 'C0mpuNube!',
     database: process.env.MYSQL_DATABASE || 'tienda',
 });
-*/
 
+
+/*
 const pool = mysql.createPool({
     host: '3.83.110.245',
     user: 'nube',
     password: 'C0mpuNube!',
     database:  'tienda',
 });
+*/
 
 export default pool;
